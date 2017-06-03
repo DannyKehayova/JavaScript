0696366ee853ab791fee562b5d023d1efea457d3
GoranYankov <goran415@abv.bg>
2017-06-04 01:11:11 +0300

    Опитмизация на 10 задача

--- HomeWork-7-ArrayMatricesExer/10.Orbit.js
+++ HomeWork-7-ArrayMatricesExer/10.Orbit.js
@@ -4,19 +4,13 @@
 
 function drawDashbord(input) {
     "use strict";
+    let [matrixRow, matrixCol, x, y] = input;
     let matrix = [];
-     let matrixRow = input[0];
-    let matrixCol= input[1];
-    let x = input[2];
-    let y = input[3];
-     for (let row = 0; row < matrixRow; row++) {
-         matrix[row] = [];
-         for (let col = 0; col < matrixCol; col++) {
-             matrix[row][col]=0;
-         }
-     }
 
-     matrix[x][y] = 1;
+    for (let row = 0; row < matrixRow; row++) {
+        matrix.push('0'.repeat(matrixCol).split('').map(Number));
+    }
+    matrix[x][y] = 1;
 
     for (let row = 0; row < matrixRow; row++) {
         for (let col = 0; col < matrixCol; col++) {
@@ -24,12 +18,9 @@ function drawDashbord(input) {
                 continue;
             }
             matrix[row][col] = (Math.max(Math.abs((row - x)), Math.abs(col - y))) + 1;
-
         }
-
     }
     matrix.forEach((e)=> console.log(e.join(' ')));
-
 }
 
-drawDashbord([3, 3, 2, 2]);
+drawDashbord([6, 5, 2, 2]);
